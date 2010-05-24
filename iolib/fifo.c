53c65e8b3da0c4ecbf558698ebfb56da2607a070
Sameer D. Sahasrabuddhe <sameerds@gajanan.(none)>
2010-05-25 02:38:58 +0530

    do not create a fifo when reading; use open instead of mkfifo to check for existing fifo; also added documentation

--- iolib/fifo.c
+++ iolib/fifo.c
@@ -2,10 +2,9 @@
 
 #include <stdio.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <inttypes.h>
+#include <sys/stat.h>           /* for mkfifo() flags */
+#include <stdlib.h>             /* for exit() */
+#include <fcntl.h>              /* for open() flags */
 
 static FILE* open_for_reading(char *id)
 {
@@ -13,18 +12,16 @@ static FILE* open_for_reading(char *id)
   void *data = NULL;
   int mkfifo_status = 0;
   
-  mkfifo_status = mkfifo(id, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-  if (mkfifo_status != 0) {
-    if (errno != EEXIST)
-      exit(0);
-  /* } else {*/
-    /* fprintf(stderr, "\ncreated fifo %s for reading.", id); */
+  /* The reader never creates a FIFO. It goes into a loop with sleep()
+     for one second, waiting for the FIFO to be created by the
+     writer. */
+  F = fopen(id, "r");
+  while (!F) {
+    sleep(1);
+    F = fopen(id, "r");
   }
 
-  F = fopen(id, "r");
-  if (!F)
-    exit(0);
-  /* fprintf(stderr, "\nopened fifo %s for reading.", id); */
+  /* fprintf(stderr, "\nopened FIFO %s for reading.", id); */
   
   return F;
 }
@@ -34,6 +31,7 @@ float read_float32(char *id)
   float f;
   FILE *F = open_for_reading(id);
   fread(&f, sizeof(float), 1, F);
+  fclose(F);
   return f;
 }
 
@@ -42,6 +40,7 @@ uint32_t read_uint32(char *id)
   uint32_t i;
   FILE *F = open_for_reading(id);
   fread(&i, sizeof(uint32_t), 1, F);
+  fclose(F);
   return i;
 }
 
@@ -49,17 +48,37 @@ static FILE* open_for_writing(char *id)
 {
   FILE *F = NULL;
   int mkfifo_status;
+  int fd;
+
+  /* Try to open a FIFO for writing, without creating it. Have to use
+     open() and not fopen() here, because the latter always creates a
+     simple file if the named object does not exist. */
+  fd = open(id, O_WRONLY);
   
-  mkfifo_status = mkfifo(id, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-  if (mkfifo_status != 0) {
-    if (errno != EEXIST)
+  if (fd == -1) {
+    if (errno == ENOENT) {
+      /* The FIFO doesn't exist, so we create it. We could have
+         directly tried mkfifo() in place of open(). This would
+         eliminate the later call to fdopen(), but the untested
+         assumption is that open() followed by fdopen() is cheaper
+         than mkfifo() when the FIFO already exists. */
+      mkfifo_status = mkfifo(id, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+      if (mkfifo_status != 0) {
+        exit(0);
+      }
+      fd = open(id, O_WRONLY);
+      if (fd == -1)             /* paranoia... */
+        exit(0);
+    } else
+      /* open() failed even when the FIFO exists. */
       exit(0);
-  /* } else { */
-    /* fprintf(stderr, "\ncreated fifo %s for writing.", id); */
   }
-  
-  F = fopen(id, "w");
-  if (!F)
+  /* fprintf(stderr, "\ncreated FIFO %s for writing.", id); */
+
+  /* Since open() returns a file descripter, we have to associate it
+     with a stream before returning. */
+  F = fdopen(fd, "w");
+  if (!F)                       /* ... bordering on OCD? */
     exit(0);
   
   return F;
