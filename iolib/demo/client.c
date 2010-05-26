e4e8d9091965f60315f161ffff4f45e630de2cd0
Sameer D. Sahasrabuddhe <sameerds@gajanan.(none)>
2010-05-26 18:12:33 +0530

    the client server demo is now protected by macros to choose gcc v/s llvm

--- iolib/demo/client.c
+++ iolib/demo/client.c
@@ -1,6 +1,9 @@
 #include "../iolib.h"
+
+#ifdef RUN
 #include <stdlib.h>
 #include <stdio.h>
+#endif
 
 int main (void)
 {
@@ -13,18 +16,27 @@ int main (void)
   while (i < 10) {
     apl = rand();
     my_ong = (float)apl;
+    
     write_uint32("apples", apl);
+    #ifdef RUN
     fprintf(stderr, "\n(%d) sent an apple: %d.", i, apl);
+    #endif
+    
     ong = read_float32("oranges");
+    #ifdef RUN
     fprintf(stderr, "\ngot an orange: %f.", ong);
+    #endif
+    
     failure |= (my_ong != ong);
     ++i;
   }
 
+  #ifdef RUN
   if (failure == 0)
     fprintf(stderr, "\nAll conversions successful.\n");
   else
     fprintf(stderr, "\nSome conversion(s) failed.\n");
+  #endif
     
-  return 0;
+  return failure;
 }
--- iolib/demo/server.c
+++ iolib/demo/server.c
@@ -1,5 +1,7 @@
 #include "../iolib.h"
+#ifdef RUN
 #include <stdio.h>
+#endif
 
 int main (void)
 {
@@ -9,12 +11,19 @@ int main (void)
   
   for (i = 0; i < 10; ++i) {
     apl = read_uint32("apples");
+    #ifdef RUN
     fprintf(stderr, "\n(%d) got an apple: %d.", i, apl);
+    #endif
+    
     ong = (float)apl;
     write_float32("oranges", ong);
+    #ifdef RUN
     fprintf(stderr, "\nsent an orange: %f.", ong);
+    #endif
   }
 
+  #ifdef RUN
   fprintf(stderr, "\n");
+  #endif
   return 0;
 }
