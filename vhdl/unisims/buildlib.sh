bfd9a3ac58367caa9da4144ec3428a39c71f146a
madhav desai <madhav@strangelove.ee.iitb.ac.in>
2012-10-13 18:41:48 +0530

    Added unisims library for post-synth simulations..

---
+++ vhdl/unisims/buildlib.sh
@@ -0,0 +1,4 @@
+ghdl -a --ieee=synopsys --work=unisim -fexplicit  unisim_VCOMP.vhd
+ghdl -a --ieee=synopsys --work=unisim -fexplicit unisim_VPKG.vhd
+ghdl -a --ieee=synopsys --work=unisim -fexplicit unisim_VITAL.vhd
+ghdl -a --ieee=synopsys --work=unisim -fexplicit unisim_SMODEL.vhd
