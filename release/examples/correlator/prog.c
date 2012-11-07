5dd396cc27973a6620fd6df0786b75694e83516e
Madhav P. Desai <madhav@ee.iitb.ac.in>
2012-11-07 23:25:22 +0530

    committing tests of fpu32.

--- release/examples/correlator/prog.c
+++ release/examples/correlator/prog.c
@@ -24,13 +24,13 @@ uint32_t global_job_counter;
 		I2_2 = (I1_2 + offset) & MASK;\
 		I2_3 = (I1_3 + offset) & MASK;\
 		B0 = B[I2_0]; B1 = B[I2_1]; B2 = B[I2_2]; B3 = B[I2_3];\
-		P0 = fpmul32(A0,B0);\
-		P1 = fpmul32(A1,B1);\
-		P2 = fpmul32(A2,B2);\
-		P3 = fpmul32(A3,B3);\
-		S0 = fpadd32(P0,P1);\
-		S1 = fpadd32(P2,P3);\
-		ret_val = fpadd32(ret_val, fpadd32(S0,S1));\
+		P0 = fpu32(A0,B0,MUL);\
+		P1 = fpu32(A1,B1,MUL);\
+		P2 = fpu32(A2,B2,MUL);\
+		P3 = fpu32(A3,B3,MUL);\
+		S0 = fpu32(P0,P1,ADD);\
+		S1 = fpu32(P2,P3,ADD);\
+		ret_val = fpu32(ret_val, fpu32(S0,S1,ADD), ADD);\
 	}}
 
 
