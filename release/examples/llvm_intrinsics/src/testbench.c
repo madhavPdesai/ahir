06e8aad083a9d642892ec0563bf614cda6ca30f2
Madhav Desai <madhav@ee.iitb.ac.in>
2014-11-20 11:57:40 +0530

    Added test case for llvm-intrinsics.. Seems to work OK.

---
+++ release/examples/llvm_intrinsics/src/testbench.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#ifdef SW
+#include <Pipes.h>
+#include <pipeHandler.h>
+#else
+#include "vhdlCStubs.h"
+#endif
+
+int main(int argc, char* argv[])
+{
+
+	uint32_t idx;
+	for(idx = 0; idx < 16; idx++)
+	{
+		write_uint32("in_pipe", idx);
+	}
+
+	fprintf(stderr, "Observed:\t");
+	for(idx = 0; idx < 16; idx++)
+	{
+		uint32_t fidx = read_uint32("out_pipe");
+		fprintf(stderr," %d", fidx);
+	}
+	fprintf(stderr,"\n");
+	fprintf(stderr,"Expected:\t 4 5 6 7 4 5 6 8 9 9 10 11 10 11 0 0\n");
+
+	return(0);
+}
+
