85320e8373936105e7fc3767a448153e1f21220c
Madhav Desai <madhav@mahishasura.(none)>
2015-09-06 11:13:03 +0530

    Bug fix. in flow-through VHDL printing of Floating point ops.

--- v2/libAhirV2/src/vcSystem.cpp
+++ v2/libAhirV2/src/vcSystem.cpp
@@ -1105,6 +1105,10 @@ void  vcSystem::Print_VHDL_Inclusions(ostream& ofile)
 
   ofile << "library ieee;\n\
 use ieee.std_logic_1164.all;\n			\
+library aHiR_ieee_proposed;\n \
+use aHiR_ieee_proposed.math_utility_pkg.all;\n \
+use aHiR_ieee_proposed.fixed_pkg.all;\n \
+use aHiR_ieee_proposed.float_pkg.all;\n \
 library ahir;\n					\
 use ahir.memory_subsystem_package.all;\n	\
 use ahir.types.all;\n				\
