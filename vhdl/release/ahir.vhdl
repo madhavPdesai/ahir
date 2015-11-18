a21186c6722de2ad883ff1ca9e790754bfcb99da
Madhav Desai <madhav@ee.iitb.ac.in>
2015-11-19 00:13:30 +0530

    Several improvements in logging.  It should be possible to debug the VHDL simulation based simply on the log file. No need to dump waveforms.
    Also fixed big bug in PipelineSynchBuffer, and
    improved PipelineRegister from the combi path pov.

--- vhdl/release/ahir.vhdl
+++ vhdl/release/ahir.vhdl
@@ -10402,9 +10402,7 @@ begin  -- default_arch
 		 	severity note;
 	end if;
 
-	if(not incr) then
-        	token_latch <= token_latch - 1;
-	end if;
+       	token_latch <= token_latch - 1;
 
       elsif incr then
 
@@ -20602,11 +20600,11 @@ begin  -- default_arch
 			when Full =>
 				read_ack_v := '1';
 				if(read_req = '1') then
+					write_ack_v := '1';
 					if(write_req = '0') then
 						next_state := Empty;
 					else
 						latch_v := '1';
-						write_ack_v := '1';
 					end if;
 				end if;
 		end case;
@@ -20639,23 +20637,10 @@ use ahir.Utilities.all;
 use ahir.BaseComponents.all;
 
 --
---
--- gets the read/write cycle into synch.
---   read_req -> read_ack 0-delay
---   write_req -> write_ack 1-delay
---   write_ack -> read_ack 0-delay.
---     (note that the last dependency will increase the
---       combinational path lengths... but will NOT create combinational loops).
---
--- In a 
---   buf0 -> buf1 -> buf2 ...
--- chain, the combinational paths in the CP will become
--- w-ack-k -|-> read-ack-k -|-> write-req-k+1 
--- read-req-k ->  read-ack-k  etc.
---
---  To cut the long path we will have to use an InterlockBuffer
--- periodically.
---
+--  A fast synch buffer.  Has  combinational paths from
+--     write-req -> write-ack
+--     read-req  -> write-ack
+--  handle with care..
 --
 entity PipelineSynchBuffer is
   generic (name : string; in_data_width: integer; out_data_width: integer);
@@ -20674,39 +20659,27 @@ architecture default_arch of PipelineSynchBuffer is
   constant min_data_width: integer := Minimum(in_data_width, out_data_width);
   signal data_register : std_logic_vector(min_data_width-1 downto 0);
   signal joined_req : boolean;
-
-  constant pmarkings: IntegerArray(1 to 2) := (1 => 0, 2 => 1);
-  constant pcapacities : IntegerArray(1 to 2) := (1 => 1, 2 => 1);
-  constant pdelays : IntegerArray(1 to 2) := (1 => 0, 2 => 0);
-  signal preds : BooleanArray (1 to 2);
-  signal read_ack_buffer, read_ack_pre_buffer: boolean;
   
 begin  -- default_arch
  
-  preds(1) <= write_req;
-  preds(2) <= read_ack_buffer;
 
   -- join.
-  reqJoin: generic_join
-		generic map (name => name & " synch-buf-join",  place_capacities => pcapacities, place_delays => pdelays,
-					place_markings => pmarkings)
-		port map(preds => preds, symbol_out => joined_req, clk => clk, reset => reset);
-
-  ackJoin: join2 generic map (name => name & " synch-buf-ack-join", bypass => true)
-			port map (pred0 => joined_req, pred1 => read_req, symbol_out => read_ack_pre_buffer,
+  reqJoin: join2 generic map (name => name & " synch-buf-req-join", bypass => true)
+			port map (pred0 => write_req, pred1 => read_req, symbol_out => joined_req,
 					clk => clk, reset => reset);
-  -- 0-delay.
+
   write_ack <= joined_req;
 
-  -- ack.
-  read_ack <= read_ack_buffer;
 
-  -- state machine.
   process(clk, reset, joined_req)
   begin
 	if(clk'event and clk = '1') then
-		read_ack_buffer <= (read_ack_pre_buffer and (reset = '0'));
-		if(read_ack_pre_buffer) then
+		if(reset = '1') then
+			read_ack <= false;
+		else
+			read_ack <= joined_req;
+		end if;
+		if(joined_req) then
 			data_register <= write_data(min_data_width-1 downto 0);	
 		end if;
 	end if;	
