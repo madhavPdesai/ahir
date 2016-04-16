6d3e2e88ced4f525ccdfa1436485d39f0e02e8dd
Madhav Desai <madhav@ee.iitb.ac.in>
2016-04-16 12:59:06 +0530

    Updated

---
+++ vhdl/ahir/operatorsV2/ap_int/PipelinedApIntOperator.vhd
@@ -0,0 +1,151 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library ahir;
+use ahir.GlobalConstants.all;
+use ahir.Types.all;
+use ahir.Subprograms.all;
+use ahir.Utilities.all;
+use ahir.BaseComponents.all;
+use ahir.ApIntComponents.all;
+
+entity PipelinedApIntOperator is
+  generic (
+      name : string;
+      operator_id : string;
+      no_arbitration: boolean := true;
+      in_data_width: integer;
+      out_data_width: integer;
+      num_reqs : integer := 3; -- how many requesters?
+      use_input_buffering: boolean := true;
+      detailed_buffering_per_input: IntegerArray;
+      detailed_buffering_per_output: IntegerArray;
+      full_rate: boolean
+    );
+  port (
+    -- req/ack follow level protocol
+    reqL                     : in BooleanArray(num_reqs-1 downto 0);
+    ackR                     : out BooleanArray(num_reqs-1 downto 0);
+    ackL                     : out BooleanArray(num_reqs-1 downto 0);
+    reqR                     : in  BooleanArray(num_reqs-1 downto 0);
+    -- input data consists of concatenated pairs of ips
+    dataL                    : in std_logic_vector((2*in_data_width*num_reqs)-1 downto 0);
+    -- output data consists of concatenated pairs of ops.
+    dataR                    : out std_logic_vector((out_data_width*num_reqs)-1 downto 0);
+    -- with dataR
+    clk, reset              : in std_logic);
+end entity;
+
+architecture Vanilla of PipelinedApIntOperator is
+
+  constant num_operands : integer := 2;
+  constant operand_width : integer := in_data_width;
+  constant iwidth : integer := 2*operand_width;
+  constant owidth : integer := out_data_width;
+  
+  constant tag_length: integer := Maximum(1,Ceil_Log2(reqL'length));
+  signal itag,otag : std_logic_vector(tag_length-1 downto 0);
+  signal ireq,iack, oreq, oack: std_logic;
+
+  constant debug_flag : boolean := global_debug_flag;
+
+  signal idata: std_logic_vector(iwidth-1 downto 0);
+  signal odata: std_logic_vector(owidth-1 downto 0);
+begin  -- Behave
+
+  assert ackL'length = reqL'length report "mismatched req/ack vectors" severity error;
+  assert ((operand_width = 32) or (operand_width = 64)) report "32/64 bit operand-support only!" severity error;
+  assert ((operator_id = "ApFloatMul") or (operator_id = "ApFloatAdd") or (operator_id = "ApFloatSub"))
+    report "operator_id must be either add or mul or sub" severity error;  
+
+  -- DebugGen: if debug_flag generate 
+    -- assert( (not ((reset = '0') and (clk'event and clk = '1') and no_arbitration)) or Is_At_Most_One_Hot(reqL))
+    -- report "in no-arbitration case, at most one request should be hot on clock edge (in SplitOperatorShared)" severity error;
+  -- end generate DebugGen;
+  NoInBuffers : if not use_input_buffering generate 
+    imux: InputMuxBase
+      generic map(iwidth => iwidth*num_reqs,
+                  owidth => iwidth, 
+                  twidth => tag_length,
+                  nreqs => num_reqs,
+                  no_arbitration => no_arbitration,
+                  registered_output => false)
+      port map(
+          reqL       => reqL,
+        ackL       => ackL,
+        reqR       => ireq,
+        ackR       => iack,
+        dataL      => dataL,
+        dataR      => idata,
+        tagR       => itag,
+        clk        => clk,
+        reset      => reset);
+   end generate NoInBuffers;
+
+  InBuffers: if use_input_buffering generate
+    imuxWithInputBuf: InputMuxWithBuffering
+      generic map(name => name & " imux " , 
+		iwidth => iwidth*num_reqs,
+                owidth => iwidth, 
+                twidth => tag_length,
+                nreqs => num_reqs,
+		buffering => detailed_buffering_per_input,
+                no_arbitration => no_arbitration,
+		full_rate => full_rate,
+                registered_output => false)
+      port map(
+        reqL       => reqL,
+        ackL       => ackL,
+        reqR       => ireq,
+        ackR       => iack,
+        dataL      => dataL,
+        dataR      => idata,
+        tagR       => itag,
+        clk        => clk,
+        reset      => reset);
+  end generate InBuffers;
+
+   pipeOp: GenericApIntArithOperator
+		generic map(op_id => operator_id,
+				tag_width => tag_length,
+				 in_operand_width => in_data_width,
+					num_non_constant_inputs => 0,	
+					   first_operand_is_constant => false,
+					    second_operand_is_constant => false,
+						constant_value => "",
+						out_result_width => out_data_width)
+		port map ( in_data => idata,
+    				out_data => odata,
+    				clk => clk,
+				reset => reset,
+    				tag_in => itag,
+    				tag_out => otag,
+    				env_rdy => ireq,
+		        	accept_rdy => oack, 
+    				op_i_rdy => iack,
+				op_o_rdy => oreq);
+
+
+  odemux: OutputDeMuxBaseWithBuffering
+    generic map (
+        name => name & " odemux ",
+  	iwidth => owidth,
+  	owidth =>  owidth*num_reqs,
+	twidth =>  tag_length,
+	nreqs  => num_reqs,
+	detailed_buffering_per_output => detailed_buffering_per_output,
+	full_rate => full_rate)
+    port map (
+      reqL   => oreq,
+      ackL   => oack,
+      dataL => odata,
+      tagL  => otag,
+      reqR  => reqR,
+      ackR  => ackR,
+      dataR => dataR,
+      clk   => clk,
+      reset => reset);
+  
+end Vanilla;
+
