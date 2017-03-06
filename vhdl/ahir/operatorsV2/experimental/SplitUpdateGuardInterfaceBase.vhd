1e5af846ff65d7d6339cbeba221242cac213a7f7
Madhav Desai <madhav@ee.iitb.ac.in>
2017-03-06 16:13:17 +0530

    Updated back to what it was.

--- vhdl/ahir/operatorsV2/experimental/SplitUpdateGuardInterfaceBase.vhd
+++ vhdl/ahir/operatorsV2/experimental/SplitUpdateGuardInterfaceBase.vhd
@@ -61,24 +61,79 @@ end entity;
 
 
 architecture Behave of SplitUpdateGuardInterfaceBase is
-	signal sr_out_sig, sa_in_sig: Boolean;
+	Type FsmState is (Idle, Busy);
+	signal fsm_state: FsmState;
+	signal ca_out_u, ca_out_d: Boolean;
+	signal sampled_guard_signal, registered_guard_signal: std_logic;
 begin
 
-	base: SplitGuardInterfaceBase
-		  generic map (name => name & "-base",
-				 buffering => buffering)
-			port map (sr_in => sr_in,
-	      				sa_out => sa_out,
-	      				sr_out => sr_out_sig,
-	      				sa_in => sa_in_sig,
-	      				cr_in => cr_in,
-	      				ca_out => ca_out,
-	      				cr_out => cr_out,
-	      				ca_in => ca_in,
-	      				guard_interface => guard_interface,
-	      				clk => clk, reset => reset);
+	-- sr/sa interface is a dummy... no need to forward to the
+	-- operator which does not care anyway.
+	--   However, the guard signal is sampled for use on the
+	--   update side.
+	process(clk,reset,sr_in)
+	begin
+		sa_out <= sr_in;
+		if(clk'event and clk = '1') then
+			if(reset = '1') then
+				registered_guard_signal <= '0';
+			elsif sr_in then
+				registered_guard_signal <= guard_interface;
+			end if;
+		end if;
+	end process;
+	sampled_guard_signal <= guard_interface when sr_in else registered_guard_signal;
 
- 	sa_in_sig <= sr_out_sig;
 	sr_out <= false;
 
+	-- update guard FSM.
+	process(clk, sampled_guard_signal, fsm_state, cr_in, ca_in)
+		variable next_state : FsmState;
+		variable cr_out_var, ca_out_var_d, ca_out_var_u: Boolean;
+	begin
+		next_state := fsm_state;
+		cr_out_var := false;
+		ca_out_var_u := false;
+		ca_out_var_d := false;
+		case fsm_state is
+			when Idle =>
+				if(cr_in) then
+					if(sampled_guard_signal  = '1') then
+						cr_out_var := true;
+						next_state := Busy;
+					else
+						-- give delayed ack, since guard is 0.
+						ca_out_var_d := true;	
+					end if;
+				end if;
+			when Busy => 
+				if(ca_in) then
+					ca_out_var_u := true;
+					if(cr_in) then
+						if(sampled_guard_signal = '1') then
+							cr_out_var := true;
+						else
+							ca_out_var_d := true;
+							next_state := Idle;
+						end if;
+					else
+						next_state := Idle;
+					end if;
+				end if;
+		end case;
+		
+		cr_out <= cr_out_var;
+		ca_out_u <= ca_out_var_u;
+
+		if(clk'event and clk = '1') then
+			if(reset = '1') then
+				fsm_state <= Idle;
+				ca_out_d <= false;
+			else 
+				fsm_state <= next_state;
+				ca_out_d <= ca_out_var_d;
+			end if;
+		end if;
+	end process;	
+	ca_out <= ca_out_u or ca_out_d;
 end Behave;
