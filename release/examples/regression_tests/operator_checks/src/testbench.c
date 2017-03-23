1296dbcdaa506bc152acb3e2432b8b5be4e03296
Madhav Desai <madhav@ee.iitb.ac.in>
2017-03-23 21:42:23 +0530

    Added a small example to check out operator VHDL.

---
+++ release/examples/regression_tests/operator_checks/src/testbench.c
@@ -0,0 +1,61 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <pthreadUtils.h>
+#include <Pipes.h>
+#include <pipeHandler.h>
+#ifndef SW
+#include "vhdlCStubs.h"
+#endif
+
+#define ORDER 16
+
+void Exit(int sig)
+{
+	fprintf(stderr, "## Break! ##\n");
+	exit(0);
+}
+	
+void Sender()
+{
+	int idx;
+	uint32_t val[ORDER];
+	for(idx = 0; idx < ORDER; idx++)
+	{
+		val[idx] = idx;
+	}
+	write_uint32_n("in_data",val,ORDER);
+}
+DEFINE_THREAD(Sender)
+
+int main(int argc, char* argv[])
+{
+	signal(SIGINT,  Exit);
+  	signal(SIGTERM, Exit);
+
+	PTHREAD_DECL(Sender);
+	PTHREAD_CREATE(Sender);
+
+	uint8_t idx;
+	uint32_t result[ORDER];
+	
+
+	read_uint32_n("out_data", result, ORDER);
+
+	for(idx = 0; idx < ORDER; idx++)
+	{
+		fprintf(stdout,"%d. Result = %x, expected %x\n", idx, result[idx], (idx + 1));
+	}
+	PTHREAD_CANCEL(Sender);
+
+	for(idx = 0; idx < ORDER; idx++)
+	{
+		write_uint32("in_data", idx);
+		uint32_t res = read_uint32("out_data");
+		fprintf(stdout,"%d. Result = %x, expected %x\n", idx, res, (idx + 1));
+	}
+
+	return(0);
+}
