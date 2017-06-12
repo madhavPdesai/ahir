ab25e6e4e23a63619335543dbdb79066c0b35ce0
Madhav Desai <madhav@ee.iitb.ac.in>
2017-06-12 16:43:22 +0530

    Updated example

--- v2/Ahir-asm/devel/examples/ToC/DoWhile/compile.sh
+++ v2/Ahir-asm/devel/examples/ToC/DoWhile/compile.sh
@@ -3,5 +3,5 @@ indent aa_c_model.c
 indent aa_c_model.h
 gcc -g -c -I$AHIR_RELEASE/include  -I./ aa_c_model.c
 gcc -g -c -I$AHIR_RELEASE/include  -I./ driver.c 
-gcc -g -o driver driver.o aa_c_model.o -L$AHIR_RELEASE/lib  -lBitVectors -lPipeHandler -lpthread 
+gcc -g -o driver driver.o aa_c_model.o -L$AHIR_RELEASE/lib  -lBitVectors -lPipeHandler -lpthread  -lSockPipes
 rm *.o *~
