73c9f63e1dc5c79a961b5b189cb45cde210ac665
Madhav Desai <madhav@ee.iitb.ac.in>
2017-08-24 23:53:09 +0530

    Inadvertent loop introduced in aggressive pipelining.  Easy fix.

--- v2/Ahir-asm/devel/src/AaStatement.cpp
+++ v2/Ahir-asm/devel/src/AaStatement.cpp
@@ -5719,8 +5719,12 @@ void AaDoWhileStatement::Write_VC_Control_Path(bool optimize_flag, ostream& ofil
 		__T("aggregated_phi_update_ack");
 
 		// do not loop-back unless all phi's have used
-		// up their triggering tokens.
-		__F("aggregated_phi_update_req", "condition_evaluated");
+		// up their triggering tokens.  Delay introduced
+		ofile << "// do not loop-back unless all phi's have used up their triggering tokens." << endl;
+		ofile << "// add delay to avoid 0-delay cycle through bypassed branch block" << endl;
+		ofile << "$T [all_phis_have_used_triggering_tokens] $delay" << endl;
+		__F("aggregated_phi_update_req", "all_phis_have_used_triggering_tokens");
+		__F("all_phis_have_used_triggering_tokens", "condition_evaluated");
 
 		//
 		// do not re-sample unless the result of the previous
