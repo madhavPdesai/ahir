116a992b2e70ba3c5d8b709ab454be633a113a92
Madhav Desai <madhav@ee.iitb.ac.in>
2017-09-13 14:39:21 +0530

    Put some keep attributes to prevent Xilinx from optimizing away a signal assigned to a constant.

--- vhdl/ahir/control-path/auto_run.vhdl
+++ vhdl/ahir/control-path/auto_run.vhdl
@@ -48,9 +48,16 @@ end auto_run;
 
 architecture default_arch of auto_run is
 
+    signal start_req_sig, fin_req_sig: std_logic;
+    attribute keep : string;
+    attribute keep of start_req_sig : signal is "true";
+    attribute keep of fin_req_sig : signal is "true";
 begin  
 
-  start_req <= '1';
-  fin_req <= '1';
+  start_req_sig <= '1';
+  fin_req_sig <= '1';
+
+  start_req <= start_req_sig;
+  fin_req <= fin_req_sig;
 
 end default_arch;
