48b15c6f41c8936c714a0c7001897321226a1eb0
Madhav Desai <madhav@ee.iitb.ac.in>
2017-10-29 23:30:45 +0530

    Added example to check functioning of call arbiter..

--- release/examples/regression_tests/calls/src/testbench.c
+++ release/examples/regression_tests/calls/src/testbench.c
@@ -11,7 +11,8 @@
 #endif
 
 #define ORDER 16
-uint32_t expected_result[ORDER];
+uint32_t expected_result_1[ORDER];
+uint32_t expected_result_2[ORDER];
 
 void Exit(int sig)
 {
@@ -20,19 +21,33 @@ void Exit(int sig)
 }
 
 	
-void Sender()
+void Sender_1()
 {
 	int idx;
 	uint32_t val[ORDER];
 	for(idx = 0; idx < ORDER; idx++)
 	{
 		val[idx] = (2*idx)+1;
-		expected_result[idx] = val[idx];
+		expected_result_1[idx] = val[idx];
 	}
-	write_uint32_n("in_data",val,ORDER);
+	write_uint32_n("in_data_1",val,ORDER);
 }
 
-DEFINE_THREAD(Sender)
+DEFINE_THREAD(Sender_1)
+
+void Sender_2()
+{
+	int idx;
+	uint32_t val[ORDER];
+	for(idx = 0; idx < ORDER; idx++)
+	{
+		val[idx] = (2*idx);
+		expected_result_2[idx] = val[idx];
+	}
+	write_uint32_n("in_data_2",val,ORDER);
+}
+
+DEFINE_THREAD(Sender_2)
 
 int main(int argc, char* argv[])
 {
@@ -40,17 +55,26 @@ int main(int argc, char* argv[])
 	signal(SIGINT,  Exit);
   	signal(SIGTERM, Exit);
 
-	PTHREAD_DECL(Sender);
-	PTHREAD_CREATE(Sender);
+	PTHREAD_DECL(Sender_1);
+	PTHREAD_DECL(Sender_2);
+
+	PTHREAD_CREATE(Sender_1);
+	PTHREAD_CREATE(Sender_2);
 
 	uint8_t idx;
 	
-	read_uint32_n("out_data",result,ORDER);
-
+	read_uint32_n("out_data_1",result,ORDER);
 	for(idx = 0; idx < ORDER; idx++)
 	{
-		fprintf(stdout,"Result = %x, expected = %x.\n", result[idx],expected_result[idx]);
+		fprintf(stdout,"Result = %x, expected = %x.\n", result[idx],expected_result_1[idx]);
 	}
-	PTHREAD_CANCEL(Sender);
+	read_uint32_n("out_data_2",result,ORDER);
+	for(idx = 0; idx < ORDER; idx++)
+	{
+		fprintf(stdout,"Result = %x, expected = %x.\n", result[idx],expected_result_2[idx]);
+	}
+
+	PTHREAD_CANCEL(Sender_1);
+	PTHREAD_CANCEL(Sender_2);
 	return(0);
 }
