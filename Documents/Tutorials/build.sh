66e39ed99d290de23471a0b419c779729c79784e
Madhav Desai <madhav@ee.iitb.ac.in>
2018-03-06 20:12:49 +0530

    Two scripts

---
+++ Documents/Tutorials/build.sh
@@ -0,0 +1,24 @@
+cd Aa
+pdflatex Aa.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
+cd DesignProblem
+pdflatex DesignProblem.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
+cd DpCpEtc
+pdflatex DpCpEtc.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
+cd Intro
+pdflatex Intro.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
+cd Optimizations
+pdflatex Optimizations.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
+cd Pipelines
+pdflatex Pipelines.tex
+rm -f *.log *.nav *.aux *.toc *.out *.snm
+cd -
---
+++ Documents/Tutorials/clean.sh
@@ -0,0 +1,18 @@
+cd Aa
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
+cd DesignProblem
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
+cd DpCpEtc
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
+cd Intro
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
+cd Optimizations
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
+cd Pipelines
+rm -f *.log *.nav *.pdf *.aux *.toc *.out *.snm
+cd -
