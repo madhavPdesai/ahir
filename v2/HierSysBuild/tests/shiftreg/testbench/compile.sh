0e0b06d11738c63cf22741c0f7b6a3a07ff6142f
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2019-10-11 10:09:50 +0530

    Updated.

--- v2/HierSysBuild/tests/shiftreg/testbench/compile.sh
+++ v2/HierSysBuild/tests/shiftreg/testbench/compile.sh
@@ -1,1 +1,1 @@
-gcc -g -o tb -I../aa2c/ main.c -L ../lib/ -lSR4 -L $AHIR_RELEASE/lib -lPipeHandlerDebugPthreads -lpthread -lBitVectors -lSockPipes
+gcc -g -o tb -I../aa2c/ main.c -L ../lib/ -lSR4 -L $AHIR_RELEASE/lib -I $AHIR_RELEASE/include  -lPipeHandlerDebugPthreads -lpthread -lBitVectors -lSockPipes
