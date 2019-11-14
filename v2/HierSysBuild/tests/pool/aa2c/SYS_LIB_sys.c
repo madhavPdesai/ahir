01fddbabc76f33b15b9f33414553f4f5116483cc
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2019-11-14 14:32:31 +0530

    Updated.

--- v2/HierSysBuild/tests/pool/aa2c/SYS_LIB_sys.c
+++ v2/HierSysBuild/tests/pool/aa2c/SYS_LIB_sys.c
@@ -4,8 +4,6 @@
 #include <rtl2AaMatcher.h>
 #include <SYS_LIB_sys.h>
 void SYS_LIB_start_daemons(FILE* fp, int trace_on) {
- register_pipe("DATA_OUT", 1, 8, 0);
-set_pipe_is_read_from("DATA_OUT");
  register_pipe("E1_IN", 1, 8, 0);
 set_pipe_is_written_into("E1_IN");
 set_pipe_is_read_from("E1_IN");
@@ -20,6 +18,8 @@ set_pipe_is_written_into("E2_OUT");
 set_pipe_is_read_from("E2_OUT");
  register_pipe("DATA_IN", 1, 8, 0);
 set_pipe_is_written_into("DATA_IN");
+ register_pipe("DATA_OUT", 1, 8, 0);
+set_pipe_is_read_from("DATA_OUT");
 SYS_LIB_sys_e1_start_daemons(fp,trace_on);
 SYS_LIB_sys_e2_start_daemons(fp,trace_on);
 SYS_LIB_sys_c_start_daemons(fp,trace_on);
