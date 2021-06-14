37a65ec564362f1f758a813f62d8cf383c2b5906
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2021-06-14 13:58:19 +0530

    Added prebuilt_ubuntu_16.04_release to git repo.

--- release/vhdl/unisims/buildlib.sh
+++
@@ -1,4 +0,0 @@
-rm -f *.cf *.o
-ghdl -a --ieee=synopsys --no-vital-checks --work=unisim -fexplicit unisim_VCOMP.vhd
-ghdl -a --ieee=synopsys --no-vital-checks --work=unisim -fexplicit unisim_VPKG.vhd
-ghdl -a --ieee=synopsys --no-vital-checks --work=unisim -fexplicit primitive/*.vhd
