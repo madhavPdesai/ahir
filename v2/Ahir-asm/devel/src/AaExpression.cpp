d4ee7f057df233320c0ba6f010925cf107f435d2
Madhav Pandurang Desai <madhav@ee.iitb.ac.in>
2023-09-16 20:14:26 +0530

    In last commit, note that double buffering of guarded full-rate unit-delay operators is only for non-pipe-reads.

--- v2/Ahir-asm/devel/src/AaExpression.cpp
+++ v2/Ahir-asm/devel/src/AaExpression.cpp
@@ -127,10 +127,14 @@ void AaExpression::Write_VC_Output_Buffering(string dpe_name, string tgt_name, o
 	if(stmt_buf > this_buffering)
 		this_buffering = stmt_buf;
 
-	if(this->Is_Part_Of_Fullrate_Pipeline())
+	//
+	// Guarded expression with unit delay and full-rate?
+	//   Double buffer!
+	//
+	if((this->Get_Guard_Expression() != NULL) && (this_buffering < 2) && !this->Is_Pipe_Read() 
+			&& (this->Get_Delay() == 1))
 	{
-		if((this_buffering < 2) && (this->Get_Delay() == 1) && 
-				(this->Get_Guard_Expression() != NULL))
+		if(this->Is_Part_Of_Fullrate_Pipeline())
 		{
 			this_buffering = 2;
 			ofile << "// Buffering of fullrate unit-delay guarded expression is set to 2." << endl;
